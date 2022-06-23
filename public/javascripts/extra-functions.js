9b2c19e981754eb24b460627360013ad334b811f
Michel Mendes <michel.gmsm@gmail.com>
2022-06-23 18:00:09 -0300

    Some minor changes, unfortunately today I didn't have much time to dedicate

--- public/javascripts/extra-functions.js
+++ public/javascripts/extra-functions.js
@@ -18,44 +18,13 @@ async function apiRequest ( reqUrl, reqMethod = 'GET' /* Default is GET */, reqB
         const jsonResponse = JSON.parse( textResponse );
 
         return jsonResponse;
-    } catch ( err ) {
+    } catch ( catchError ) {
         return {
             error: true,
             message: textResponse,
-            errorMessage: err
+            errorMessage: catchError
         }
     }
 }
-
-
-// function apiRequest ( reqUrl, reqMethod = 'GET' /* Default is GET */, reqBody = {}) {
-    
-//     var reqHeaders = new Headers();
-//     reqHeaders.append("Content-Type", "application/json");
-    
-//     let reqInit = {
-//         method: reqMethod,
-//         headers: reqHeaders,
-//         body: JSON.stringify( reqBody )
-//     };
-
-//     let myRequest = new Request( reqUrl, reqInit );
-    
-//     fetch( myRequest )
-//     .then ( function(response) { return response.text() } )
-//     .then ( function(text) {
-//         try {
-//             const jsonResponse = JSON.parse( text );
-
-//             return jsonResponse;
-//         }
-//         catch(err) {
-//             return {
-//                 error: true,
-//                 message: err
-//             };
-//         }
-//     })
-// }
 
 export { apiRequest };
