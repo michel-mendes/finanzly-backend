7918a03fac48d752de32f2dafcfe3b82e9f88603
Michel Mendes <michel.gmsm@gmail.com>
2023-05-08 17:46:53 -0300

    chore: creates "Upload" router and controller

---
+++ src/controllers/UploadController.ts
@@ -0,0 +1,3 @@
+import { Response, NextFunction } from "express";
+import { IAuthRequest } from "../types/auth-request";
+import { Parser } from "papaparse";
---
+++ src/routes/UploadRouter.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const uploadRouter = Router()
+
+// Router implementations here
+
+export { uploadRouter }
